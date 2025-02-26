d2d6d2a0e2b3c644b72ae2912f1425a7f3917f91
marianinhamon1411 <mariana-1411@hotmail.com>
2025-02-26 17:45:28 -0300

    Adicionar arquivos de projeto.

---
+++ MauiAppMinhasCompras/Resources/Views/EditarProduto.xaml.cs
@@ -0,0 +1,9 @@
+namespace MauiAppMinhasCompras.Resources.Views;
+
+public partial class EditarProduto : ContentPage
+{
+	public EditarProduto()
+	{
+		InitializeComponent();
+	}
+}
---
+++ MauiAppMinhasCompras/Resources/Views/ListaProduto.xaml.cs
@@ -0,0 +1,9 @@
+namespace MauiAppMinhasCompras.Resources.Views;
+
+public partial class ListaProduto : ContentPage
+{
+	public ListaProduto()
+	{
+		InitializeComponent();
+	}
+}
